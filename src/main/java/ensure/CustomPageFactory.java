4d22ae0da57118922186075adbf738882a390dc5
Vinh Nguyen <npvinh140589@gmail.com>
2019-11-21 17:44:46 +0700

    Change class name

--- src/main/java/ensure/CustomPageFactory.java
+++ src/main/java/ensure/CustomPageFactory.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebElement;
 
 import java.lang.reflect.Field;
 
-public class CustomPage {
+public class CustomPageFactory {
 
-  private CustomPage() {
+  private CustomPageFactory() {
     throw new IllegalArgumentException("Custom Page Factory class");
   }
 
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,7 +1,7 @@
 package pages;
 
 import com.google.inject.Inject;
-import ensure.CustomPage;
+import ensure.CustomPageFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -14,6 +14,6 @@ public class HomePage extends BasePage {
   @Inject
   public HomePage(WebDriver driver) throws IllegalAccessException {
     super(driver);
-    CustomPage.initElements(driver, this);
+    CustomPageFactory.initElements(driver, this);
   }
 }
