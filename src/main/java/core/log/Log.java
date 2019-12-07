adf2ff6030be7db2a43e99a0011c5b687cdb5f9d
Vinh Nguyen <npvinh140589@gmail.com>
2019-12-07 19:04:19 +0700

    Update

--- src/main/java/core/log/Log.java
+++ src/main/java/core/log/Log.java
@@ -27,19 +27,19 @@ public class Log {
                 break;
             case FAIL:
                 LOGGER.error(message);
-                ExtentTestManager.getTest().log(Status.FAIL, message);
+//                ExtentTestManager.getTest().log(Status.FAIL, message);
                 break;
             case PASS:
                 LOGGER.info(message);
-                ExtentTestManager.getTest().log(Status.PASS, message);
+//                ExtentTestManager.getTest().log(Status.PASS, message);
                 break;
             case ERROR:
                 LOGGER.error(message);
-                ExtentTestManager.getTest().log(Status.ERROR, message);
+//                ExtentTestManager.getTest().log(Status.ERROR, message);
                 break;
             case SKIPPED:
                 LOGGER.warn(message);
-                ExtentTestManager.getTest().log(Status.SKIP, message);
+//                ExtentTestManager.getTest().log(Status.SKIP, message);
                 break;
             default:
                 throw new IllegalArgumentException("No log type(INFO/FAIL/PASS/ERROR) is provided");
