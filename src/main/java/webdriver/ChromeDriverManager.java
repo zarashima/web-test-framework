b7daa3c91c741aa2a52f6e2224fc98dcd7f5386b
Vinh Nguyen <vinh.nguyen@v-key.com>
2020-02-12 16:08:44 +0700

    Handle 'Chrome failed to start: exited abnormally' error

--- src/main/java/webdriver/ChromeDriverManager.java
+++ src/main/java/webdriver/ChromeDriverManager.java
@@ -2,11 +2,16 @@ package webdriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverManager extends DriverManager {
 
   public void createDriver() {
     WebDriverManager.chromedriver().setup();
-    driver = new ChromeDriver();
+    ChromeOptions chromeOptions = new ChromeOptions();
+    chromeOptions.addArguments("--no-sandbox");
+    chromeOptions.addArguments("--headless");
+    chromeOptions.addArguments("--disable-dev-shm-usage");
+    driver = new ChromeDriver(chromeOptions);
   }
 }
