3abbf02be4281efe6df3cab9237641db6de30c99
Vinh Nguyen <vinh.nguyen@v-key.com>
2020-04-10 10:26:57 +0700

    Fix code reviews

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,12 +2,11 @@ package pages;
 
 import com.google.inject.Inject;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.PropertyUtils;
 
 class BasePage {
 
-  @Inject
-  BasePage(WebDriver driver) {
-  }
+	@Inject
+	BasePage(WebDriver driver) {
+
+	}
 }
