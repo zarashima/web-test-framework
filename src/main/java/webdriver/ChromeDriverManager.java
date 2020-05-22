2c4213b329777d2a7843602f86bac7b759369c03
Vinh Nguyen <npvinh140589@gmail.com>
2020-05-22 22:28:15 +0700

    Seperate chrome execution for different purposes

--- src/main/java/webdriver/ChromeDriverManager.java
+++ src/main/java/webdriver/ChromeDriverManager.java
@@ -4,16 +4,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ChromeDriverManager extends DriverManager {
 
 	public void createDriver(DesiredCapabilities desiredCapabilities) {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--no-sandbox");
-		chromeOptions.addArguments("--headless");
-		chromeOptions.addArguments("--disable-dev-shm-usage");
-		desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-		driver = new ChromeDriver(desiredCapabilities);
+		String runWhere = System.getenv("RUNWHERE");
+		switch (runWhere) {
+			case "pipeline":
+				chromeOptions.addArguments("--no-sandbox", "--headless", "--disable-dev-shm-usage");
+				desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+				driver = new ChromeDriver(desiredCapabilities);
+				break;
+			case "container":
+				String seleniumHubUrl = "http://localhost:4444/wd/hub";
+				chromeOptions.addArguments("--whitelisted-ips");
+				chromeOptions.addArguments("--no-sandbox");
+				try {
+					driver = new RemoteWebDriver(new URL(seleniumHubUrl), chromeOptions);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+				break;
+			default:
+				driver = new ChromeDriver();
+				break;
+		}
 	}
 }
