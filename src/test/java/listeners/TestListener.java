11ce9086b589af11555f597cf13a3db55e94f55a
Vinh Nguyen <npvinh140589@gmail.com>
2020-05-30 16:23:57 +0700

    Remove Kibana integration

--- src/test/java/listeners/TestListener.java
+++ src/test/java/listeners/TestListener.java
@@ -9,14 +9,9 @@ import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import status.ResultsSender;
-import status.TestStatus;
-import utils.ExecutionUtils;
-import utils.PropertyUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.Objects;
 
@@ -25,21 +20,16 @@ public class TestListener implements ITestListener {
 	@Inject
 	WebDriver driver;
 
-	TestStatus testStatus;
-
 	@Override
 	public synchronized void onTestStart(ITestResult result) {
-		this.testStatus = new TestStatus();
 	}
 
 	@Override
 	public synchronized void onTestSuccess(ITestResult result) {
-		this.sendStatus(result,"PASS");
 	}
 
 	@Override
 	public synchronized void onTestFailure(ITestResult result) {
-		this.sendStatus(result,"FAIL");
 		ITestContext context = Objects.requireNonNull(result).getTestContext();
 		driver = (WebDriver) context.getAttribute("driver");
 		try {
@@ -56,7 +46,6 @@ public class TestListener implements ITestListener {
 
 	@Override
 	public synchronized void onTestSkipped(ITestResult result) {
-		this.sendStatus(result,"SKIPPED");
 	}
 
 	@Override
@@ -69,16 +58,5 @@ public class TestListener implements ITestListener {
 
 	@Override
 	public synchronized void onFinish(ITestContext context) {
-	}
-
-	private void sendStatus(ITestResult iTestResult, String status) {
-		this.testStatus.setTestClass(iTestResult.getTestClass().getTestName());
-		this.testStatus.setDescription(iTestResult.getMethod().getDescription());
-		this.testStatus.setStatus(status);
-		this.testStatus.setExecutionDate(LocalDateTime.now().toString());
-		if (!status.equals("PASSED"))
-			this.testStatus.setStatusMessage(iTestResult.getThrowable().getMessage());
-		if (ExecutionUtils.getParameter("kibana.integration").equals("true"))
-			ResultsSender.send(this.testStatus);
 	}
 }
