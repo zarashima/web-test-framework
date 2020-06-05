893af425deb3a4797e67b2d0bc2684088c685714
Vinh Nguyen <npvinh140589@gmail.com>
2020-06-05 21:22:58 +0700

    Encapsulate verification

--- src/main/java/keywords/Verification.java
+++ src/main/java/keywords/Verification.java
@@ -1,16 +1,28 @@
 package keywords;
 
+import com.google.inject.Inject;
+import ensure.Wait;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.LogUtils;
 import utils.WebElementUtils;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Verification {
 
+	@Inject
+	WebDriver driver;
+
+	@Inject
+	public Verification(WebDriver driver) {
+		this.driver = driver;
+	}
+
 	protected WebElement element;
 
-	public boolean verifyElementDisplayed(WebElement element) {
-		LogUtils.info("Verify " + WebElementUtils.getElementXpathInfo(element) + " is displayed");
-		return element.isDisplayed();
+	public void verifyEqual(Object actual, Object expect) {
+		LogUtils.info("Verify " + actual + " equal to " + expect);
+		assertThat(actual).isEqualTo(expect);
 	}
 
 }
--- src/test/java/tests/SignInTest.java
+++ src/test/java/tests/SignInTest.java
@@ -3,8 +3,6 @@ package tests;
 import org.testng.annotations.Test;
 import utils.PropertyUtils;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class SignInTest extends BaseTest {
 
 	@Test(description = "Verify invalid message is displayed when using invalid email and password")
@@ -12,6 +10,6 @@ public class SignInTest extends BaseTest {
 		browserKeywords.goTo(PropertyUtils.getInstance().getAutHomepage());
 		homePage.goToSignInPage();
 		signInPage.signIn("admin", "password");
-		assertThat(signInPage.getErrorMessage()).isEqualTo("Invalid email");
+		verificationKeywords.verifyEqual(signInPage.getErrorMessage(), "Invalid email");
 	}
 }
