48241612d11c83b38f2494e6004672bc24429c86
Vinh Nguyen <npvinh140589@gmail.com>
2020-06-15 16:21:34 +0700

    Revert sign in test

--- src/test/java/tests/SignInTest.java
+++ src/test/java/tests/SignInTest.java
@@ -11,19 +11,11 @@ import utils.PropertyUtils;
 		priority = Priority.MEDIUM,
 		createdBy = "vinh.nguyen")
 public class SignInTest extends BaseTest {
-	@Test(dataProvider = "sampleData", description = "Verify invalid message is displayed when using invalid email and password")
-	public void verifySignIn_invalidEmailPassword_shouldPromptInvalidMessage(String sampleData) {
+	@Test(description = "Verify invalid message is displayed when using invalid email and password")
+	public void verifySignIn_invalidEmailPassword_shouldPromptInvalidMessage() {
 		browserKeywords.goTo(PropertyUtils.getInstance().getAutHomepage());
-		Assert.assertEquals(1,2);
 		homePage.goToSignInPage();
 		signInPage.signIn("admin", "password");
 		verificationKeywords.verifyEqual(signInPage.getErrorMessage(), "Invalid email");
-	}
-
-	@DataProvider(name = "sampleData")
-	public static Object[][] getSampledata() {
-		return new Object[][] {
-				{"aaaa"}
-		};
 	}
 }
