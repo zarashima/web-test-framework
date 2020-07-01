f38ca69505e0a70f050ced1e17f5848ebd8296d3
Vinh Nguyen <npvinh140589@gmail.com>
2020-07-01 13:27:36 +0700

    Update bash file

--- run-tests.sh
+++ run-tests.sh
@@ -7,3 +7,15 @@ docker build -t=vinh/frameworkdocker .
 
 echo "Run tests"
 docker-compose up -d --force-recreate
+
+echo "Docker compose logs"
+docker-compose logs -t
+
+LINE_TO_CONTAIN="exited with code"
+SLEEP_TIME=10
+until [[ $(cat $(docker-compose logs -t > output.log) | grep "${LINE_TO_CONTAIN}") ]]
+do
+    sleep ${SLEEP_TIME}
+done
+
+docker-compose logs --tail=100
