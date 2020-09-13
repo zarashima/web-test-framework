06e1a4108ded380437bb4f6438010dbabf63fd40
Vinh Nguyen <npvinh140589@gmail.com>
2020-09-13 12:07:22 +0700

    Fix compile issue

--- src/main/java/utils/WebElementUtils.java
+++ src/main/java/utils/WebElementUtils.java
@@ -1,14 +1,10 @@
 package utils;
 
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
-
 public class WebElementUtils {
 	private WebElementUtils() {
 	}
 
-	public static String getElementXpathInfo(WebElement element) {
+	public static synchronized String getElementXpathInfo(Object element) {
 		String[] elementInfo;
 		if (element.toString().contains("xpath: "))
 			elementInfo = element.toString().split("xpath: ");
@@ -17,12 +13,4 @@ public class WebElementUtils {
 		return elementInfo[elementInfo.length - 1];
 	}
 
-	public static String getElementsXpathInfo(List<WebElement> elements) {
-		String[] elementInfo;
-		if (elements.toString().contains("xpath: "))
-			elementInfo = elements.toString().split("xpath: ");
-		else
-			elementInfo = elements.toString().split("css selector: ");
-		return elementInfo[elementInfo.length - 1];
-	}
 }
